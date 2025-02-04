ee503aa19922e5a1990af5bddc37f585d8ebaa25
Palakon Phomjul <122507427+Godtheking1@users.noreply.github.com>
2025-02-04 12:10:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// ให้ Express ให้บริการไฟล์ Static จากโฟลเดอร์ 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-
 // เมื่อเข้าถึง root (/) ให้แสดงไฟล์ dashboard.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+    res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
 
 // ตั้งค่าพอร์ตให้ใช้จากตัวแปรแวดล้อม หรือใช้ 10000 เป็นค่าเริ่มต้น
