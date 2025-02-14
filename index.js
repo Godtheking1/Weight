4f47563cc9853573e88146a0dce03ab20617ed69
Palakon Phomjul <122507427+Godtheking1@users.noreply.github.com>
2025-02-14 09:48:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+require('dotenv').config();
+const app = require('./app');
+const db = require('./db');
+
+const PORT = process.env.PORT || 4000;
+
+async function startServer() {
+	//on database connection
+	db.once('open', () => console.log('connected to the databse'));
+
+	app.listen(PORT, () => {
+		console.log(`Server started at http://localhost:${PORT}`);
+	});
+}
+
+startServer();
